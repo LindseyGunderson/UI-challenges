864d44a0bb3f2f4b57ab01a016080db654564017
LindseyGunderson <65924727+LindseyGunderson@users.noreply.github.com>
2021-09-18 17:29:02 -0700

    Setting up files for contact form
    Setting up files and components.

---
+++ create-account-form/src/components/Contact.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+
+
+`;
+
+const Contact = () => {
+    return (
+        <Container>
+            Nailed it.
+        </Container>
+    )
+}
+
+export default Contact
