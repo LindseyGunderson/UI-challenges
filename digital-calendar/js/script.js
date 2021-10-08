8d20e42324f4b2a4de94d3b914bbb24d44387ed5
LindseyGunderson <65924727+LindseyGunderson@users.noreply.github.com>
2021-10-08 14:20:26 -0700

    Add styling for calendar
    Added styling for current date and overview of calendar

---
+++ digital-calendar/js/script.js
@@ -0,0 +1,29 @@
+var weekNameList = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+
+// var month = document.getElementById("monthName"),
+//     week = document.getElementById("weekName"),
+//     day = document.getElementById("dayName"),
+//     year = document.getElementById("year");
+
+// function getCurrentDate() {
+
+//     let date = new Date();
+
+//     let getDayNumber = date.getDate();
+
+//     let getMonth = date.getMonth();
+
+//     let getDayName = date.toLocaleDateString(land, {weekday: 'long'});
+
+//     let getMonthName = date.toLocaleDateString(land, {month: 'long'});
+
+//     let getYear = date.getFullYear();
+
+//     month.innerHTML = getMonthName;
+//     week.innerHTML = getDayName;
+//     day.innerHTML = getDayNumber;
+//     year.innerHTML = getYear;
+
+// }
+
+// getCurrentDate();
